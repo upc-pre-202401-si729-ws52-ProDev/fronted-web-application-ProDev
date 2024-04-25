fd8a24c18611c7c2415ecbedd89877b2feeb045d
fdaniel13s <u20221c628@upc.edu.pe>
2024-04-24 23:52:37 -0500

    feat: footer-component structure and styles

---
+++ src/app/public/components/footer-content/footer-content.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-content',
+  standalone: true,
+  imports: [],
+  templateUrl: './footer-content.component.html',
+  styleUrl: './footer-content.component.css'
+})
+export class FooterContentComponent {
+
+}
