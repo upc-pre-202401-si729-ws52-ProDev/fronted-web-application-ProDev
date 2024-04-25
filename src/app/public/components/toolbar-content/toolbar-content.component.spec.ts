bee42f0090081616fe6ba4a88d6ff609876e5212
fdaniel13s <u20221c628@upc.edu.pe>
2024-04-24 23:52:13 -0500

    feat: toolbar-component done

---
+++ src/app/public/components/toolbar-content/toolbar-content.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ToolbarContentComponent } from './toolbar-content.component';
+
+describe('ToolbarContentComponent', () => {
+  let component: ToolbarContentComponent;
+  let fixture: ComponentFixture<ToolbarContentComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ToolbarContentComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(ToolbarContentComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
