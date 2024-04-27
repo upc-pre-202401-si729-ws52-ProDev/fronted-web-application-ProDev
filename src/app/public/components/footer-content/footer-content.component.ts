4527417523aedc16eb5358f05e14ebd94cebb3c8
unset <unset>
2024-04-27 12:34:16 -0500

    feat: buttons with router of toolbar.

--- src/app/public/components/footer-content/footer-content.component.ts
+++ src/app/public/components/footer-content/footer-content.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {MatButton, MatButtonModule} from "@angular/material/button";
 
 @Component({
   selector: 'app-footer-content',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButtonModule
+  ],
   templateUrl: './footer-content.component.html',
   styleUrl: './footer-content.component.css'
 })
