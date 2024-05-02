05a2c0da88f9cd9b6c6436df2c4d8452bd0f38d4
fdaniel13s <u20221c628@upc.edu.pe>
2024-05-01 19:38:40 -0500

    chore: add baseURL and createDonation function.

--- src/app/donation-management/services/donation-api.service.ts
+++ src/app/donation-management/services/donation-api.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {Subject, tap} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class DonationApiService {
 
-  baseUrl = 'assets/data/donations.json';
+  baseUrl = 'http://localhost:3000/donations';
+
+  private donationCreatedSource = new Subject<void>();
+  donationCreated$ = this.donationCreatedSource.asObservable();
 
   constructor(private http: HttpClient) { }
 
@@ -14,4 +18,11 @@ export class DonationApiService {
     return this.http.get<any>(this.baseUrl);
   }
 
+  createDonation(donation: any){
+    return this.http.post(this.baseUrl, donation).pipe(
+      tap(() => {
+        this.donationCreatedSource.next();
+      })
+    );
+  }
 }
