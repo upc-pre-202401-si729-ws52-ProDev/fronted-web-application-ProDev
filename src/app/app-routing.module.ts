fc24e9154015f8f00778b3ea588f86e2a4d294ea
fdaniel13s <u20221c628@upc.edu.pe>
2024-05-02 15:55:43 -0500

    chore: add routing to test profile.edit.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from "@angular/router";
+import { routes } from "./app.routes";
+import {EditProfileComponent} from "./ecomarket/components/profile/edit-profile/edit-profile.component";
 
 @NgModule({
-  declarations: [
-
-  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forRoot(routes),
+    EditProfileComponent
+  ],
+  exports: [RouterModule],
+  declarations: [
   ]
 })
 export class AppRoutingModule { }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
 import {DonationsPageComponent} from "./donation-management/components/donations-page/donations-page.component";
+import {EditProfileComponent} from "./ecomarket/components/profile/edit-profile/edit-profile.component";
 
 export const routes: Routes = [
   {
     path: 'donations',
     component: DonationsPageComponent
+  },
+
+  {
+    path:'profile/edit-profile',
+    component: EditProfileComponent
   }
 ];
