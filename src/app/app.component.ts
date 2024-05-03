62d46e54851833c74c16398f74a0701b50d412fc
fdaniel13s <u20221c628@upc.edu.pe>
2024-05-03 07:42:33 -0500

    refactor: fix important errors about action of move directory.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {DonationListComponent} from "./donation-management/components/donation-list/donation-list.component";
-import {DonationsPageComponent} from "./donation-management/components/donations-page/donations-page.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DonationListComponent, DonationsPageComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import { Routes } from '@angular/router';
-import {DonationsPageComponent} from "./donation-management/components/donations-page/donations-page.component";
+import {
+  DonationsPageComponent
+} from "./ecomarket/pages/donation-management/components/donations-page/donations-page.component";
 import {EditProfileComponent} from "./ecomarket/components/profile/edit-profile/edit-profile.component";
 
 export const routes: Routes = [
