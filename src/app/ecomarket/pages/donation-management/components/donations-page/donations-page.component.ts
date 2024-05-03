21fc82a95fd4c2191f96408ee3c3b7fa07bffd57
fdaniel13s <u20221c628@upc.edu.pe>
2024-05-03 07:50:09 -0500

    fix: fix imports.

--- src/app/ecomarket/pages/donation-management/components/donations-page/donations-page.component.ts
+++ src/app/ecomarket/pages/donation-management/components/donations-page/donations-page.component.ts
@@ -1,18 +1,18 @@
 import { Component } from '@angular/core';
 import {DonationListComponent} from "../donation-list/donation-list.component";
 import {DonationFormComponent} from "../donation-form/donation-form.component";
-import {ToolbarContentComponent} from "../../../public/components/toolbar-content/toolbar-content.component";
+import {ToolbarContentComponent} from "../../../../../public/components/toolbar-content/toolbar-content.component";
 
 
 
 @Component({
   selector: 'app-donations-page',
   standalone: true,
-    imports: [
-        DonationListComponent,
-        DonationFormComponent,
-        ToolbarContentComponent
-    ],
+  imports: [
+    DonationListComponent,
+    DonationFormComponent,
+    ToolbarContentComponent
+  ],
   templateUrl: './donations-page.component.html',
   styleUrl: './donations-page.component.css'
 })
