a68d22b1f0cfaaa46f939acf6e7cf1883c54ca5c
fdaniel13s <u20221c628@upc.edu.pe>
2024-05-03 01:28:07 -0500

    refactor: fix something.

--- src/app/public/components/toolbar-content/toolbar-content.component.ts
+++ src/app/public/components/toolbar-content/toolbar-content.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatIcon} from "@angular/material/icon";
 import {MatButton, MatIconButton} from "@angular/material/button";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-toolbar-content',
@@ -10,7 +11,8 @@ import {MatButton, MatIconButton} from "@angular/material/button";
     MatToolbarModule,
     MatIcon,
     MatIconButton,
-    MatButton
+    MatButton,
+    RouterLink
   ],
   templateUrl: './toolbar-content.component.html',
   styleUrl: './toolbar-content.component.css'
