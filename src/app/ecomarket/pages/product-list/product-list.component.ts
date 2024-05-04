7141366f9aec3447f36f95592b0c88e9835fa36e
AnkSaldarriaga <136540757+AnkSaldarriaga@users.noreply.github.com>
2024-05-04 12:39:36 -0500

    feat: update

--- src/app/ecomarket/pages/product-list/product-list.component.ts
+++ src/app/ecomarket/pages/product-list/product-list.component.ts
@@ -7,6 +7,8 @@ import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import {ToolbarContentComponent} from "../../../public/components/toolbar-content/toolbar-content.component";
 import {ProfileApiService} from "../../services/profile-services/profile-api.service";
+import {FormControl, ReactiveFormsModule} from '@angular/forms';
+import { startWith } from 'rxjs/operators';
 
 @Component({
   selector: 'app-product-list',
@@ -17,6 +19,8 @@ import {ProfileApiService} from "../../services/profile-services/profile-api.ser
 })
 export class ProductListComponent implements OnInit {
   rows: any[] = [];
+  filteredRows: any[] = [];
+  searchControl = new FormControl();
 
   constructor(private productsApiService: ProfileApiService) {
   }
@@ -26,11 +30,19 @@ export class ProductListComponent implements OnInit {
       (data: any) => {
         // Dependiendo de cómo esté estructurado tu JSON, necesitarás acceder a la propiedad adecuada
         this.rows = data; // Ajusta esto según la estructura de tu JSON
+        this.filteredRows = this.rows;
+
       },
       error => {
         console.log('Error obteniendo productos:', error);
       }
     );
+
+    this.searchControl.valueChanges
+      .pipe(startWith(''))
+      .subscribe(value => {
+        this.filteredRows = this.rows.filter(product => product.name.toLowerCase().includes(value.toLowerCase()));
+      });
   }
 }
 
