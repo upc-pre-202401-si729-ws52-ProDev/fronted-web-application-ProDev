9a34b798afbbdcd37a1b3aec9cc2b087ffa13a3a
fdaniel13s <u20221c628@upc.edu.pe>
2024-05-24 23:21:45 -0500

    feat(add products to cart): added function tu add products.

--- src/app/ecomarket/pages/product-list-customers/product-list-customers.component.ts
+++ src/app/ecomarket/pages/product-list-customers/product-list-customers.component.ts
@@ -8,12 +8,12 @@ import { MatIconModule } from '@angular/material/icon';
 import {ProductsApiService} from "../../services/donation-services/products-api.service";
 import {ToolbarContentComponent} from "../../../public/components/toolbar-content/toolbar-content.component";
 import {ToolbarCustomerComponent} from "../../../public/components/toolbar-customer/toolbar-customer.component";
-import {FormControl, ReactiveFormsModule} from '@angular/forms';
+import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { startWith } from 'rxjs/operators';
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [MatCardModule, MatFormFieldModule, MatInputModule, MatButtonModule, CommonModule, MatIconModule, ToolbarContentComponent, ToolbarCustomerComponent, ReactiveFormsModule],
+  imports: [MatCardModule, MatFormFieldModule, MatInputModule, MatButtonModule, CommonModule, MatIconModule, ToolbarContentComponent, ToolbarCustomerComponent, ReactiveFormsModule, FormsModule],
   templateUrl: 'product-list-customers.component.html',
   styleUrl: 'product-list-customers.component.css'
 })
@@ -21,6 +21,9 @@ export class ProductListCustomersComponent implements OnInit {
   rows: any[] = [];
   filteredRows: any[] = [];
   searchControl = new FormControl();
+  quantity: number = 0;
+  productsAtCart: any[] = [];
+
 
   constructor(private productsApiService: ProductsApiService) {
   }
@@ -42,5 +45,20 @@ export class ProductListCustomersComponent implements OnInit {
         this.filteredRows = this.rows.filter(product => product.name.toLowerCase().includes(value.toLowerCase()));
       });
   }
+
+  addToCart(product: any, quantity: number){
+    console.log('Añadiendo al carrito:', product , quantity);
+
+    // Aquí implemento la lógica para añadir al carrito
+    // por ejemplo, podrías guardar los productos en un arreglo
+    // y luego mostrarlos en un componente de carrito
+    this.productsAtCart.push({product, quantity});
+
+    //Limpiando la variable quantity
+    this.quantity = 0;
+
+  }
+
+
 }
 
