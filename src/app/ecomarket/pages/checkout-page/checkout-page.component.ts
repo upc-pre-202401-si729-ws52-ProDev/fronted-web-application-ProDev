c7a1c97feeaed66e9e191ba721c98a8032deb6bb
fdaniel13s <u20221c628@upc.edu.pe>
2024-06-20 22:22:38 -0500

    feat: added styles to checkout page.

--- src/app/ecomarket/pages/checkout-page/checkout-page.component.ts
+++ src/app/ecomarket/pages/checkout-page/checkout-page.component.ts
@@ -1,12 +1,16 @@
 import {Component, OnInit} from '@angular/core';
 import {NgForOf} from "@angular/common";
 import {CartService} from "../../services/shopping-cart-services/cart.service";
+import {ToolbarCustomerComponent} from "../../../public/components/toolbar-customer/toolbar-customer.component";
+import {MatCard, MatCardModule} from "@angular/material/card";
 
 @Component({
   selector: 'app-checkout-page',
   standalone: true,
   imports: [
-    NgForOf
+    NgForOf,
+    ToolbarCustomerComponent,
+    MatCardModule
   ],
   templateUrl: './checkout-page.component.html',
   styleUrl: './checkout-page.component.css'
@@ -15,12 +19,15 @@ export class CheckoutPageComponent implements OnInit{
   cartItems:any = []; // Aquí se almacenarán los productos del carrito
   selectedPaymentMethod = null; // Aquí se almacenará el método de pago seleccionado
   paymentMethods = ['Tarjeta de crédito', 'Paypal', 'Transferencia bancaria']; // Métodos de pago disponibles
+  total:any = 0; // Aquí se almacenará el total de la compra
+
 
   constructor(private cartService:CartService) {
   }
 
   ngOnInit() {
     this.cartItems = this.cartService.getCartItems();
+    //TODO LLAMAR AL SHOPPING CART PARA OBTENER LOS CART ITEMS Y TOTAL
   }
 
   selectPaymentMethod(method:any) {
