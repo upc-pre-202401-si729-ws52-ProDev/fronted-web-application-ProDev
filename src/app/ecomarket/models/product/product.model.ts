a82772889cd613f444743b7d52e4dc59cb13b947
AnkSaldarriaga <136540757+AnkSaldarriaga@users.noreply.github.com>
2024-06-27 10:07:47 -0500

    feat: add in the main

---
+++ src/app/ecomarket/models/product/product.model.ts
@@ -0,0 +1,11 @@
+export interface Product {
+  id?: string;
+  name: string;
+  stock: number;
+  price: number;
+  type: string;
+  expirationDate: string;
+  urlImage: string;
+  description: string;
+  user: string;
+}
---
+++ src/app/ecomarket/pages/product-add/product-add.component.ts
@@ -0,0 +1,64 @@
+import {Component, OnInit} from '@angular/core';
+import {MatButton} from "@angular/material/button";
+import {MatCard, MatCardContent, MatCardHeader, MatCardSubtitle} from "@angular/material/card";
+import {MatFormField, MatLabel} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+import {FormBuilder, FormGroup, ReactiveFormsModule} from "@angular/forms";
+import { Product } from "../../models/product/product.model";
+import { ProfileApiService } from "../../services/profile-services/profile-api.service";
+import {Router} from "@angular/router";
+import {AuthserviceService} from "../../services/authentication/authservice.service";
+
+@Component({
+  selector: 'app-product-add',
+  standalone: true,
+    imports: [
+        MatButton,
+        MatCard,
+        MatCardContent,
+        MatCardHeader,
+        MatCardSubtitle,
+        MatFormField,
+        MatInput,
+        MatLabel,
+        ReactiveFormsModule
+    ],
+  templateUrl: './product-add.component.html',
+  styleUrl: './product-add.component.css'
+})
+
+export class ProductAddComponent implements OnInit {
+  productForm!: FormGroup;
+  constructor(private profileApiService: ProfileApiService, private formBuilder: FormBuilder, private router: Router, private authService: AuthserviceService) {
+  }
+
+  ngOnInit() {
+    this.productForm = this.formBuilder.group({
+      name: [''],
+      stock: [''],
+      price: [''],
+      type: [''],
+      expirationDate: [''],
+      urlImage: [''],
+      description: [''],
+      user: ['']
+    });
+    const currentUser = this.authService.getCurrentUser();
+    this.productForm.patchValue({user: currentUser});
+  }
+
+  onSubmit() {
+
+    if (this.productForm.valid) {
+      this.profileApiService.addProduct(this.productForm.value).subscribe(
+        response => {
+          console.log('Product added successfully', response);
+          this.router.navigate(['/product-list']);
+        },
+        error => {
+          console.error(error);
+        }
+      );
+    }
+  }
+}
